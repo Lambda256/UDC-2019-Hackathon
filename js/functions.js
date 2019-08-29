d5884e9d38a4cb14d8158b8d2cfb61891f4e01a0
ricegood <yeonjaekoo@gmail.com>
2019-08-29 16:16:18 +0900

    Logo top button

--- js/functions.js
+++ js/functions.js
@@ -1,3 +1,9 @@
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
 function go() {
   document.getElementById("body2").style.display = "";
   document.getElementById("emptydiv").style.display = "";
