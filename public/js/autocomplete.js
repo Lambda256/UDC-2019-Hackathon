7db84194c55aff4e74acf7f3c064db12accf3f36
ricegood <951227@gmail.com>
2019-08-31 13:03:12 +0900

    Autocomplete by reading csv

--- public/js/autocomplete.js
+++ public/js/autocomplete.js
@@ -1,89 +1,201 @@
 // Retrieved from http://jsfiddle.net/e6220t92/2/
+const fileUrl = '../src/stations.csv' // provide file location
+fetch(fileUrl)
+   .then( r => r.text() )
+   .then( t => CSVToArray(t) )
+   .then( arr => autocomplete(arr) )
 
-var arr = ["Afghanistan", "Albania", "Algeria", "Andorra", "Angola", "Anguilla", "Antigua &amp; Barbuda", "Argentina", "Armenia", "Aruba", "Australia", "Austria", "Azerbaijan", "Bahamas", "Bahrain", "Bangladesh", "Barbados", "Belarus", "Belgium", "Belize", "Benin", "Bermuda", "Bhutan", "Bolivia", "Bosnia &amp; Herzegovina", "Botswana", "Brazil", "British Virgin Islands", "Brunei", "Bulgaria", "Burkina Faso", "Burundi", "Cambodia", "Cameroon", "Cape Verde", "Cayman Islands", "Chad", "Chile", "China", "Colombia", "Congo", "Cook Islands", "Costa Rica", "Cote D Ivoire", "Croatia", "Cruise Ship", "Cuba", "Cyprus", "Czech Republic", "Denmark", "Djibouti", "Dominica", "Dominican Republic", "Ecuador", "Egypt", "El Salvador", "Equatorial Guinea", "Estonia", "Ethiopia", "Falkland Islands", "Faroe Islands", "Fiji", "Finland", "France", "French Polynesia", "French West Indies", "Gabon", "Gambia", "Georgia", "Germany", "Ghana", "Gibraltar", "Greece", "Greenland", "Grenada", "Guam", "Guatemala", "Guernsey", "Guinea", "Guinea Bissau", "Guyana", "Haiti", "Honduras", "Hong Kong", "Hungary", "Iceland", "India", "Indonesia", "Iran", "Iraq", "Ireland", "Isle of Man", "Israel", "Italy", "Jamaica", "Japan", "Jersey", "Jordan", "Kazakhstan", "Kenya", "Kuwait", "Kyrgyz Republic", "Laos", "Latvia", "Lebanon", "Lesotho", "Liberia", "Libya", "Liechtenstein", "Lithuania", "Luxembourg", "Macau", "Macedonia", "Madagascar", "Malawi", "Malaysia", "Maldives", "Mali", "Malta", "Mauritania", "Mauritius", "Mexico", "Moldova", "Monaco", "Mongolia", "Montenegro", "Montserrat", "Morocco", "Mozambique", "Namibia", "Nepal", "Netherlands", "Netherlands Antilles", "New Caledonia", "New Zealand", "Nicaragua", "Niger", "Nigeria", "Norway", "Oman", "Pakistan", "Palestine", "Panama", "Papua New Guinea", "Paraguay", "Peru", "Philippines", "Poland", "Portugal", "Puerto Rico", "Qatar", "Reunion", "Romania", "Russia", "Rwanda", "Saint Pierre &amp; Miquelon", "Samoa", "San Marino", "Satellite", "Saudi Arabia", "Senegal", "Serbia", "Seychelles", "Sierra Leone", "Singapore", "Slovakia", "Slovenia", "South Africa", "South Korea", "Spain", "Sri Lanka", "St Kitts &amp; Nevis", "St Lucia", "St Vincent", "St. Lucia", "Sudan", "Suriname", "Swaziland", "Sweden", "Switzerland", "Syria", "Taiwan", "Tajikistan", "Tanzania", "Thailand", "Timor L'Este", "Togo", "Tonga", "Trinidad &amp; Tobago", "Tunisia", "Turkey", "Turkmenistan", "Turks &amp; Caicos", "Uganda", "Ukraine", "United Arab Emirates", "United Kingdom", "Uruguay", "Uzbekistan", "Venezuela", "Vietnam", "Virgin Islands (US)", "Yemen", "Zambia", "Zimbabwe", "Seoul National Univ.", "Sadang"];
-var input = document.getElementsByClassName("autocomplete-input")
-var optionsVal = document.getElementsByClassName("autocomplete-list")
+function autocomplete(arr) {
+  var input = document.getElementsByClassName("autocomplete-input")
+  var optionsVal = document.getElementsByClassName("autocomplete-list")
 
-input[0].addEventListener('keyup', function(){
-    show(0);
-});
-optionsVal[0].onclick = function () {
-    setVal(this, 0);
-};
+  input[0].addEventListener('keyup', function(){
+      show(0);
+  });
+  optionsVal[0].onclick = function () {
+      setVal(this, 0);
+  };
 
-input[1].addEventListener('keyup', function(){
-    show(1);
-});
-optionsVal[1].onclick = function () {
-    setVal(this, 1);
-};
+  input[1].addEventListener('keyup', function(){
+      show(1);
+  });
+  optionsVal[1].onclick = function () {
+      setVal(this, 1);
+  };
 
-//Use this function to replace potential characters that could break the regex
-RegExp.escape = function (s) {
-    return s.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&');
-};
-//shows the list
-function show(idx) {
-    var dropdown = document.getElementsByClassName("autocomplete-dropdown")[idx]
-    dropdown.style.display = 'none';
+  //Use this function to replace potential characters that could break the regex
+  RegExp.escape = function (s) {
+      return s.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&');
+  };
 
-    optionsVal[idx].options.length = 0;
+  //shows the list
+  function show(idx) {
+      var dropdown = document.getElementsByClassName("autocomplete-dropdown")[idx]
+      dropdown.style.display = 'none';
 
-    if (input[idx].value) {
-        dropdown.style.display = '';
-        optionsVal[idx].size = 3;
-        var textCountry = input[idx].value;
+      optionsVal[idx].options.length = 0;
 
-        for (var i = 0; i < arr.length; i++) {
-            var testableRegExp = new RegExp(RegExp.escape(textCountry), "i");
-            if (arr[i].match(testableRegExp)) {
-                addValue(arr[i], arr[i], idx);
+      if (input[idx].value) {
+          dropdown.style.display = '';
+          optionsVal[idx].size = 3;
+          var textCountry = input[idx].value;
+
+          for (var i = 0; i < arr.length; i++) {
+              var testableRegExp = new RegExp(RegExp.escape(textCountry), "i");
+              if (arr[i][3].match(testableRegExp)) {
+                  addValue(arr[i][3], arr[i][3], idx);
+              }
+          }
+          /*
+          if (typeof dropdown.children[0].children[0] == 'undefined') {
+            console.log("NULL")
+          } else {
+            console.log(dropdown.children[0].children[0].innerText)
+          }
+          */
+
+          var size = dropdown.children[0].children;
+          if (size.length > 0)
+          {
+             var defaultSize = 27;
+             if (size.length == 1)
+             {
+                defaultSize = 35;
+             }
+             else if (size.length < 5)
+             {
+                defaultSize *= size.length;
+             }
+             else
+             {
+                defaultSize *= 5;
+             }
+             dropdown.children[0].style.height = defaultSize + "px";
+          }
+          else
+          {
+            // there is no item
+            dropdown.style.display = 'none';
+          }
+
+      }
+  }
+
+  function addValue(text, val, idx) {
+      var createOptions = document.createElement('option');
+      optionsVal[idx].appendChild(createOptions);
+      createOptions.text = text;
+      createOptions.value = val;
+  }
+
+  //Settin the value in the box by firing the click event
+  function setVal(selectedVal, idx) {
+      input[idx].value = selectedVal.value;
+      document.getElementsByClassName("autocomplete-dropdown")[idx].style.display = 'none';
+  }
+}
+
+// https://stackoverflow.com/questions/14446447/how-to-read-a-local-text-file
+function readTextFile(file)
+{
+    var rawFile = new XMLHttpRequest();
+    rawFile.open("GET", file, false);
+    rawFile.onreadystatechange = function ()
+    {
+        if(rawFile.readyState === 4)
+        {
+            if(rawFile.status === 200 || rawFile.status == 0)
+            {
+                var allText = rawFile.responseText;
+                alert(allText);
             }
         }
-        /*
-        if (typeof dropdown.children[0].children[0] == 'undefined') {
-          console.log("NULL")
-        } else {
-          console.log(dropdown.children[0].children[0].innerText)
-        }
-        */
-
-        var size = dropdown.children[0].children;
-        if (size.length > 0)
-        {
-           var defaultSize = 27;
-           if (size.length == 1)
-           {
-              defaultSize = 35;
-           }
-           else if (size.length < 5)
-           {
-              defaultSize *= size.length;
-           }
-           else
-           {
-              defaultSize *= 5;
-           }
-           dropdown.children[0].style.height = defaultSize + "px";
-        }
-        else
-        {
-          // there is no item
-          dropdown.style.display = 'none';
-        }
-
     }
+    rawFile.send(null);
 }
 
-function addValue(text, val, idx) {
-    var createOptions = document.createElement('option');
-    optionsVal[idx].appendChild(createOptions);
-    createOptions.text = text;
-    createOptions.value = val;
-}
+// ref: http://stackoverflow.com/a/1293163/2343
+// This will parse a delimited string into an array of
+// arrays. The default delimiter is the comma, but this
+// can be overriden in the second argument.
+function CSVToArray( strData, strDelimiter ){
+    // Check to see if the delimiter is defined. If not,
+    // then default to comma.
+    strDelimiter = (strDelimiter || ",");
 
-//Settin the value in the box by firing the click event
-function setVal(selectedVal, idx) {
-    input[idx].value = selectedVal.value;
-    document.getElementsByClassName("autocomplete-dropdown")[idx].style.display = 'none';
+    // Create a regular expression to parse the CSV values.
+    var objPattern = new RegExp(
+        (
+            // Delimiters.
+            "(\\" + strDelimiter + "|\\r?\\n|\\r|^)" +
+
+            // Quoted fields.
+            "(?:\"([^\"]*(?:\"\"[^\"]*)*)\"|" +
+
+            // Standard fields.
+            "([^\"\\" + strDelimiter + "\\r\\n]*))"
+        ),
+        "gi"
+        );
+
+
+    // Create an array to hold our data. Give the array
+    // a default empty first row.
+    var arrData = [[]];
+
+    // Create an array to hold our individual pattern
+    // matching groups.
+    var arrMatches = null;
+
+
+    // Keep looping over the regular expression matches
+    // until we can no longer find a match.
+    while (arrMatches = objPattern.exec( strData )){
+
+        // Get the delimiter that was found.
+        var strMatchedDelimiter = arrMatches[ 1 ];
+
+        // Check to see if the given delimiter has a length
+        // (is not the start of string) and if it matches
+        // field delimiter. If id does not, then we know
+        // that this delimiter is a row delimiter.
+        if (
+            strMatchedDelimiter.length &&
+            strMatchedDelimiter !== strDelimiter
+            ){
+
+            // Since we have reached a new row of data,
+            // add an empty row to our data array.
+            arrData.push( [] );
+
+        }
+
+        var strMatchedValue;
+
+        // Now that we have our delimiter out of the way,
+        // let's check to see which kind of value we
+        // captured (quoted or unquoted).
+        if (arrMatches[ 2 ]){
+
+            // We found a quoted value. When we capture
+            // this value, unescape any double quotes.
+            strMatchedValue = arrMatches[ 2 ].replace(
+                new RegExp( "\"\"", "g" ),
+                "\""
+                );
+
+        } else {
+
+            // We found a non-quoted value.
+            strMatchedValue = arrMatches[ 3 ];
+
+        }
+
+        // Now that we have our value string, let's add
+        // it to the data array.
+        arrData[ arrData.length - 1 ].push( strMatchedValue );
+    }
+
+    // Return the parsed data.
+    return( arrData );
 }
