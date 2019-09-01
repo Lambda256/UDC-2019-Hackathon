76f8ebefe02cbe85520f7aecd051025e8b4199a8
eric-yoo <ajoije729@gmail.com>
2019-09-01 23:42:58 +0900

    make mymap global

--- public/js/functions.js
+++ public/js/functions.js
@@ -4,7 +4,7 @@
 // Retrieved from http://jsfiddle.net/e6220t92/2/
 
 var targets =[];
-
+var mymap = L.map('mapid');
 const fileUrl = '../src/stations.csv' // provide file location
 fetch(fileUrl)
    .then( r => r.text() )
@@ -260,7 +260,7 @@ function autocomplete(arr) {
     long_mean /= 10;
     
     // 지도 객체 추가
-    var mymap = L.map('mapid').setView([lat_mean,long_mean], 14);
+    mymap.setView([lat_mean,long_mean], 14);
     L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
       attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
       maxZoom: 18,
