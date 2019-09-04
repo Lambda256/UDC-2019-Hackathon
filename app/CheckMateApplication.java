8f0bea83180fb8cf266902a272ffcee7714a3a69
DongHyun KAL LEE <ldh1428a@sogang.ac.kr>
2019-09-04 21:58:08 +0900

    Add files via upload

---
+++ app/CheckMateApplication.java
@@ -0,0 +1,13 @@
+package me.kalriz.checkmate.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckMateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheckMateApplication.class, args);
+	}
+
+}
