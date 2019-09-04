6a38c5e52d443f9c338af66bebdbe7a328259e0b
ggomaeng <sungwoopark95@gmail.com>
2019-09-04 18:07:44 +0900

    Work on App Context

---
+++ web/src/contexts/AppContext.js
@@ -0,0 +1,59 @@
+import React, { Component } from "react";
+import api from "utils/api";
+
+const AppContext = React.createContext();
+const { Provider, Consumer } = AppContext;
+
+class AppProvider extends Component {
+  state = {
+    timeSold: 10,
+    updatingTime: false,
+    currentCampaign: null
+  };
+
+  componentDidMount() {
+  }
+
+  updateState = (key, value) => {
+    this.setState({ [key]: value });
+  };
+
+  setTimeSold = async timeSold => {
+    const start = this.state.timeSold;
+    const end = timeSold;
+    var range = end - start;
+    if(range === 0) return;
+    await this.setState({ updatingTime: true, timeSold });
+    const meridianLabel = document.querySelector(".meridianLabel");
+    var current = start;
+    var increment = end > start ? 1 : -1;
+    if(this.interval) clearInterval(this.interval);
+    this.interval = setInterval(() => {
+      current += increment;
+      meridianLabel.textContent = `${current} HOURS`;
+
+      if (current === end) {
+        this.setState({ updatingTime: false });
+        clearInterval(this.interval);
+      }
+    }, .5);
+  };
+
+  render() {
+    return (
+      <Provider
+        value={{
+          ...this.state,
+          updateState: this.updateState,
+          setTimeSold: this.setTimeSold,
+        }}
+      >
+        {this.props.children}
+      </Provider>
+    );
+  }
+}
+
+export { AppProvider, Consumer as AppConsumer };
+
+export default AppContext;
