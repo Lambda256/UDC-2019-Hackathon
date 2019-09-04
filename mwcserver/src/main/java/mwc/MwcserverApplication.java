82880d50c588ea2bcc4da8ff52fb1c91ef371b39
신정호 <sjh8341@gmail.com>
2019-09-04 14:15:16 +0900

    초기 설정

---
+++ mwcserver/src/main/java/mwc/MwcserverApplication.java
@@ -0,0 +1,13 @@
+package mwc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MwcserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MwcserverApplication.class, args);
+	}
+
+}
