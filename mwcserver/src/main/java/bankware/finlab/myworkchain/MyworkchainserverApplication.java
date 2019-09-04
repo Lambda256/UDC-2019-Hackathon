ad3f6e9723736a7315556b6874f6cbd7e875c590
신정호 <sjh8341@gmail.com>
2019-09-05 08:05:16 +0900

    delete unused file, link error fixed

--- mwcserver/src/main/java/bankware/finlab/myworkchain/MyworkchainserverApplication.java
+++ mwcserver/src/main/java/bankware/finlab/myworkchain/MyworkchainserverApplication.java
@@ -1,17 +1,22 @@
-package mwc;
+package bankware.finlab.myworkchain;
 
 import java.sql.SQLException;
 
 import org.h2.tools.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class MwcserverApplication {
+public class MyworkchainserverApplication extends SpringBootServletInitializer {
 
+//	@Override
+//    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+//        return application.sources(MyworkchainserverApplication.class);
+//    }
 	public static void main(String[] args) {
-		SpringApplication.run(MwcserverApplication.class, args);
+		SpringApplication.run(MyworkchainserverApplication.class, args);
 	}
 	@Bean(initMethod = "start", destroyMethod = "stop")
 	public Server inMemoryH2DatabaseaServer() throws SQLException {
