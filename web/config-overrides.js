c6c00e13c05a0812606c8c0eee70d8b5c01d5a38
ggomaeng <sungwoopark95@gmail.com>
2019-09-04 15:50:39 +0900

    Config override + jsconfig.json

---
+++ web/config-overrides.js
@@ -0,0 +1,16 @@
+const { override, fixBabelImports, addLessLoader } = require('customize-cra');
+
+module.exports = override(
+  fixBabelImports('import', {
+    libraryName: 'antd',
+    libraryDirectory: 'es',
+    style: true
+  }),
+  addLessLoader({
+    javascriptEnabled: true,
+    modifyVars: {
+      "@primary-color": "#4a4a4a",
+      "@font-family": "'Open Sans', sans-serif",
+    }
+  })
+);
